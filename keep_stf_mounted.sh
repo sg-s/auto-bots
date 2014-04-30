7a3de1b4564f4e85f3c61fe20d49f2f4e4b5c835
Srinivas Gorur-Shandilya <code@srinivas.gs>
2014-04-29 22:38:45 -0400

    fixed a bug where scripts run by cron would fail because cron runs in a sparse environment

--- keep_stf_mounted.sh
+++ keep_stf_mounted.sh
@@ -2,13 +2,12 @@
 # This neat shell script runs a app that mounts an network volume when it detects that 
 # 1) it is on the correct WiFi and
 # 2) the volume is not actually present 
-if [ $(networksetup -getairportnetwork en0 | grep "secret_internet" | wc -l) -eq 1 ]
+export PATH=$PATH:/usr/sbin/
+
+if [ $(airport -I | grep "secret_internet" | wc -l) -gt 0 ] && [ $(ls /Volumes/ | grep "Red October" | wc -l) -lt 1 ]
 then
-	# we are connected to a wifi network called "secret_internet"
-	if [ $(ls /Volumes/ | grep "Red October" | wc -l) -eq 0 ]
-	then
-		# our network share is not mounted
-		open -n ./mount_stf.app --args -AppCommandLineArg
-	fi
+	open -n /code/auto-bots/mount_stf.app --args -AppCommandLineArg
+
 fi
 
+env > ~/env_dump.txt
