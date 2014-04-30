b24dfd51dfbc383182c57a2f71c86274a5e55771
Srinivas Gorur-Shandilya <code@srinivas.gs>
2014-04-29 22:39:28 -0400

    added a link explaining cron paths

--- keep_stf_mounted.sh
+++ keep_stf_mounted.sh
@@ -2,6 +2,9 @@
 # This neat shell script runs a app that mounts an network volume when it detects that 
 # 1) it is on the correct WiFi and
 # 2) the volume is not actually present 
+# why do we add this to the path?
+# see this:
+# http://stackoverflow.com/questions/2388087/how-to-get-cron-to-call-in-the-correct-paths
 export PATH=$PATH:/usr/sbin/
 
 if [ $(airport -I | grep "secret_internet" | wc -l) -gt 0 ] && [ $(ls /Volumes/ | grep "Red October" | wc -l) -lt 1 ]
