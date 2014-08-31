d63330820db7c4fc65da8e99319c63237ba3f030
Srinivas Gorur-Shandilya <code@srinivas.gs>
2014-08-31 13:51:16 -0400

    huge improvements to share.sh

--- share.sh
+++ share.sh
@@ -7,12 +7,6 @@
 # % This work is licensed under the Creative Commons Attribution-NonCommercial-ShareAlike 4.0 International License. 
 # % To view a copy of this license, visit http://creativecommons.org/licenses/by-nc-sa/4.0/.
 
-# compress chosen folder
-folder_name=$1
-zip_name=$1".zip"
-echo "Compressing into" $zip_name
-zip -r -X -q $zip_name $1 # -X flag removes junk that Mac OS X adds
-
 # make a random string
 index=0
 str=""
@@ -20,16 +14,21 @@ for i in {a..z}; do arr[index]=$i; index=`expr ${index} + 1`; done
 for i in {A..Z}; do arr[index]=$i; index=`expr ${index} + 1`; done
 for i in {0..9}; do arr[index]=$i; index=`expr ${index} + 1`; done
 for i in {1..6}; do str="$str${arr[$RANDOM%$index]}"; done
-str=html/$str
 
+# compress chosen folder
+folder_name=$1
+zip_name=$str".zip"
+echo "Compressing into" $zip_name
+zip -r -X -q $zip_name $1 # -X flag removes junk that Mac OS X adds
 
-# assume login ID (not password!) is stored in ~/server_id
-login_id=$(<~/server_id)
-ssh $login_id mkdir $str
+# get the login name from some file (assumes you have public-key SSH set up)
+login_id=$(<~/.server_id)
 
-# now copy the zip there
-scp $zip_name $login_id:$str/
+# copy to server
+scp $zip_name $login_id:/Library/WebServer/Documents/
 
-# return a link we can copy and paste
-echo $str/$zip_name
+# copy public link to clipboard 
+echo $login_id/$zip_name | pbcopy
+echo "Link copied to clipboard"
+
 
